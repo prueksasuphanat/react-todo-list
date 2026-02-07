68557d0b8beda3b5ee5737a0c887fdda79af0faa
pruek <suphanat.pruek@gmail.com>
2026-02-08 01:45:02 +0700

    fix: 🐛 git commit

--- src/features/dashboard/pages/Dashboard.tsx
+++ src/features/dashboard/pages/Dashboard.tsx
@@ -1,5 +1,18 @@
+const mockItem = Array.from({ length: 100 }, (_, index) => ({
+  id: index + 1,
+  title: `Task ${index + 1}`,
+  description: `Description for Task ${index + 1}`,
+  status: index % 3 === 0 ? 'Completed' : 'In Progress',
+}))
+
 function Dashboard() {
-  return <>dashboard p1</>
+  return (
+    <div className="flex flex-col! gap-2 mb-2">
+      {mockItem.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Dashboard
